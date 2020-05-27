99a24b7bdd5c38a70a88edb22cfd8153e34fc895
ronghui.zou <ronghui.zou@sunyard.com>
2020-05-27 16:32:50 +0800

    终止更新 - 已移交至 https://github.com/zouRongHui/program

---
+++ README.md
@@ -0,0 +1,1 @@
+# 终止更新 - 已移交至 https://github.com/zouRongHui/program
--- src/main/java/org/rone/study/springBoot/Application.java
+++ src/main/java/org/rone/study/springBoot/Application.java
@@ -1,13 +1,20 @@
 package org.rone.study.springBoot;
 
 import org.mybatis.spring.annotation.MapperScan;
+import org.rone.study.springBoot.filter.FirstFilter;
+import org.rone.study.springBoot.filter.SecondFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @MapperScan("org.rone.study.springBoot.mapper")
 //@EnableScheduling
+//通过注解注册Servlet、Filter、Listener
+@ServletComponentScan
 public class Application {
 
 	public static void main(String[] args) {
@@ -18,4 +25,30 @@ public class Application {
 		System.out.println("==========================================================================================");
 		System.out.println("------------------------------------------------------------------------------------------");
 	}
+
+//	/**
+//	 * 通过配置类注册filter
+//	 * @Author Rone    2020/1/9
+//	 * @return
+//	 */
+//	@Bean
+//	public FilterRegistrationBean registerFirstFilter() {
+//		FilterRegistrationBean bean = new FilterRegistrationBean(new FirstFilter());
+//		bean.addUrlPatterns("/*");
+//		bean.setOrder(1);
+//		return bean;
+//	}
+//
+//	/**
+//	 * 通过配置类注册filter
+//	 * @Author Rone    2020/1/9
+//	 * @return
+//	 */
+//	@Bean
+//	public FilterRegistrationBean registerSecondFilter() {
+//		FilterRegistrationBean bean = new FilterRegistrationBean(new SecondFilter());
+//		bean.addUrlPatterns("/*");
+//		bean.setOrder(1);
+//		return bean;
+//	}
 }
